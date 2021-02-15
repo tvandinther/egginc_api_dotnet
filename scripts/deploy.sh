f100ad69c57eee701c901cc178f42ffa064e4974
tvandinther <tvandinther@gmail.com>
2021-02-16 10:20:57 +1300

    fixed deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
-﻿gcloud functions deploy egginc-api-dotnet --entry-point HttpFunction.Function --runtime dotnet3 --trigger-http --env-vars-file .env.yaml --allow-unauthenticated
+﻿#!/usr/bin/env bash
+
+gcloud functions deploy egginc-api-dotnet --entry-point HttpFunction.Function --runtime dotnet3 --trigger-http --env-vars-file .env.yaml --allow-unauthenticated
